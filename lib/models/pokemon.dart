7622791edd6022b47aefd05c3f97118da0993440
brendonHale <garrett.hale88@gmail.com>
2026-09-16 13:47:44 -0600

    Created Pokemon constructor, updated environment sdk

---
+++ lib/models/pokemon.dart
@@ -0,0 +1,23 @@
+class Pokemon {
+
+  static const List<String> _validPokemonTypes = [
+    "normal",
+    "fire",
+    "water",
+    "electric",
+    "grass",
+    "ice",
+    "fighting",
+    "poison",
+    "ground",
+    "flying",
+    "psychic",
+    "bug",
+    "rock",
+    "ghost",
+    "dragon",
+    "dark",
+    "steel",
+    "fairy"
+  ];
+}
